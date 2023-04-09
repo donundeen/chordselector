c1148a30ddde5d9cd06da2d2fa19f3e9b74c7e3b
Don Undeen <donundeen@gmail.com>
2023-04-09 19:28:04 -0400

    new notes

--- d3fifthsCircle.js
+++ d3fifthsCircle.js
@@ -33,7 +33,10 @@ TODOs:
 
 const width = 450,
   height = 450,
-  margin = 40;
+  MajorMargin = 0,
+  MinorMargin = 75,
+  MajorHole = 150,
+  MinorHole = 75;
 
 
 let currentRotation = 0;
@@ -46,7 +49,16 @@ const width = "90%",
     margin = 40;
 */
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-const radius = Math.min(width, height) / 2 - margin;
+const MajorRadius = Math.min(width, height) / 2 - MajorMargin;
+const MinorRadius = Math.min(width, height) / 2 - MinorMargin;
+
+
+Array.prototype.rotateRight = function( n ) {
+  this.unshift.apply( this, this.splice( n, this.length ) );
+  return this;
+}
+
+
 
 // append the svg object to the div called 'my_dataviz'
 const svg = d3
@@ -54,6 +66,8 @@ const svg = d3
   .append("svg")
 //  .attr("width", width)
 //.attr("height", height)
+.attr("style","position:absolute;z-index:500;")
+
 .attr("width", "90%")
 .attr("height", "90%")
 .attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
@@ -69,9 +83,27 @@ d3.select("#mainsvg")
   .attr("id", "textg");
 
 
+const svg2 = d3
+  .select("#mainwindow")
+  .append("svg")
+//  .attr("width", width)
+//.attr("height", height)
+.attr("style","position:absolute;z-index:200;")
+.attr("width", "90%")
+.attr("height", "90%")
+.attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
+.attr('preserveAspectRatio', 'xMinYMid')
+.attr("id", "mainsvg2")
+.attr("transform","rotate(-15)")
+.append("g")
+.attr("id","slicesg2")
+.attr("transform", `translate(${width / 2},${height / 2})`);
+
+
 // Create dummy data
 //const data = { a: 9, b: 20, c: 30, d: 8, e: 12 };
-const data = { 1: 30, 2: 30, 3: 30, 4: 30, 5: 30, 6: 30, 7: 30, 8: 30, 9: 30, 10: 30, 11: 30, 12: 30 };
+const MajorData = { 1: 30, 2: 30, 3: 30, 4: 30, 5: 30, 6: 30, 7: 30, 8: 30, 9: 30, 10: 30, 11: 30, 12: 30 };
+const MinorData = { 1: 30, 2: 30, 3: 30, 4: 30, 5: 30, 6: 30, 7: 30, 8: 30, 9: 30, 10: 30, 11: 30, 12: 30 };
 const noteCircle = ["C","F","Bb","Eb", "Ab", "Db","Gb","B","E","A","D","G"];
 // set the color scale
 const color = d3
@@ -80,25 +112,26 @@ const color = d3
 
 // Compute the position of each group on the pie:
 const pie = d3.pie().value((d) => d[1]);
-const arc = d3.arc()
-  .innerRadius(100)         // This is the size of the donut hole
-  .outerRadius(radius)
+const MajorArc = d3.arc()
+  .innerRadius(MajorHole)         // This is the size of the donut hole
+  .outerRadius(MajorRadius)
 
-console.log(Object.entries(data));
+const MinorArc = d3.arc()
+  .innerRadius(MinorHole)         // This is the size of the donut hole
+  .outerRadius(MinorRadius)
 
-const data_ready = pie(Object.entries(data));
-
-console.log(data_ready);
+  const MajorData_ready = pie(Object.entries(MajorData));
+  const MinorData_ready = pie(Object.entries(MinorData));
 
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
 svg
-  .selectAll('whatever')
-  .data(data_ready)
+  .selectAll('#mainsvg')
+  .data(MajorData_ready)
   .join('path')
-  .attr('d', arc)
+  .attr('d', MajorArc)
   .attr('fill', d => { console.log(d); return color(d.data[0]) })
 //  .attr("data-note", d => d.data[0])
-  .attr("id", d=>"pos_"+d.data[0])
+  .attr("id", d=>"maj_pos_"+d.data[0])
   .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
@@ -108,7 +141,28 @@ svg
     console.log(d3this.attr("data-note"));
     let note = d3this.data()[0].data[0];  // this is weird, but gets us there...
     console.log(d3this.data()[0].data);
-    assignNotesToSlots(d3this.attr("data-note"));    
+    setRoot(d3this.attr("data-note"));   
+  })
+
+
+  svg2
+  .selectAll('#mainsvg2')
+  .data(MinorData_ready)
+  .join('path')
+  .attr('d', MinorArc)
+  .attr('fill', d => { console.log(d); return color(d.data[0]) })
+//  .attr("data-note", d => d.data[0])
+  .attr("id", d=>"min_pos_"+d.data[0])
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+  .on("click", function (element) {
+    console.log("clicked");
+    let d3this = d3.select(this)
+    console.log(d3this.attr("data-note"));
+    let note = d3this.data()[0].data[0];  // this is weird, but gets us there...
+    console.log(d3this.data()[0].data);
+    setRoot(d3this.attr("data-note"));   
   })
 
 /*
@@ -119,12 +173,12 @@ pie.click(function(datum){
 
 
 // add the text and other stuff to each pie element
-data_ready.forEach(function (d, i) {
+MajorData_ready.forEach(function (d, i) {
   console.log(d)
   console.log(i)
   let label = d.data[0]
-  let parentid = "pos_"+label;
-  let coords = arc.centroid(d);
+  let parentid = "maj_pos_"+label;
+  let coords = MajorArc.centroid(d);
   console.log(coords);
   let x = coords[0] + width/2;// 225;
   let y = coords[1] + height/2 //225;
@@ -132,7 +186,7 @@ data_ready.forEach(function (d, i) {
 //  d3.select("#mainsvg").append("text")
   d3.select("#textg").append("text")
     .attr("x", x).attr("y", y)
-    .attr("id", "text_"+label)
+    .attr("id", "maj_text_"+label)
     .attr("class","keepupright")
     .attr("text-anchor", "middle").attr("alignment-baseline", "middle")
     .attr("transform", "rotate(15, "+x+","+y+")")
@@ -144,23 +198,71 @@ data_ready.forEach(function (d, i) {
     });
 })
 
-assignNotesToSlots("C");
+MinorData_ready.forEach(function (d, i) {
+  console.log(d)
+  console.log(i)
+  let label = d.data[0]
+  let parentid = "min_pos_"+label;
+  let coords = MinorArc.centroid(d);
+  console.log(coords);
+  let x = coords[0] + width/2;// 225;
+  let y = coords[1] + height/2 //225;
+  let rotation = d['startAngle'] / Math.PI / 2
+//  d3.select("#mainsvg").append("text")
+  d3.select("#textg").append("text")
+    .attr("x", x).attr("y", y)
+    .attr("id", "min_text_"+label)
+    .attr("class","keepupright")
+    .attr("text-anchor", "middle").attr("alignment-baseline", "middle")
+    .attr("transform", "rotate(15, "+x+","+y+")")
+    .text(label) 
+    .on("click", function (element) {
+      console.log("lettter clicked");
+      console.log(d3.select("#"+parentid));
+      d3.select("#"+parentid).dispatch('click');    
+    });
+})
 
 
-function assignNotesToSlots(rootNote){
-  let noteindex = noteCircle.indexOf(rootNote);
+
+
+setRoot("C");
+
+
+
+
+function setRoot(rootNote){
+  noteCircle.rotateRight(noteCircle.indexOf(rootNote));
+  assignNotesToSlots(); 
+}
+
+function assignNotesToSlots(){
+  let noteindex = 0;
   let pos = 1;
   while(pos <= 12){
-    let posid = "#text_"+pos;
-    let text = noteCircle[noteindex % 12];
+    let posid = "#maj_text_"+pos;
+    let minposid = "#min_text_"+pos;
+    let majortext = noteCircle[noteindex % 12];
+    let minortext = noteCircle[(noteindex + 9) % 12];
     d3.select(posid)
-    .text(text);
-    let parentid = "#pos_"+pos;
+    .text(majortext);
+    let parentid = "#maj_pos_"+pos;
     d3.select(parentid)
-    .attr("data-note", text);
+    .attr("data-note", majortext);
+
+    d3.select(minposid)
+    .text(minortext);
+    let minparentid = "#min_pos_"+pos;
+    d3.select(minparentid)
+    .attr("data-note", minortext);
+
+
     noteindex++;
     pos++;
   }
+
+  
+
 }
 
 
